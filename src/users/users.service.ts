aa85addddfbf308c74c69dc38bb9d6583af676e7
Dang Giang <dqgiang6@gmail.com>
2025-05-27 13:43:14 +0700

    first commit

---
+++ src/users/users.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@nestjs/common';
+export type User = {
+    userId: number;
+    username: string;
+    password: string;
+}
+const users: User[] = [
+    {
+        userId: 1,
+        username: 'john',
+        password: 'changeme',
+    },
+    {
+        userId: 2,
+        username: 'chris',
+        password: '123',
+    },
+];
+
+@Injectable()
+export class UsersService {
+    async findUserByName(username: string): Promise<User | undefined> {
+        return users.find((user) => user.username === username);
+    }
+}

5c4c52ba50289e5f6d59ba31e9a4e30e63f5132b
Dang Giang <dqgiang6@gmail.com>
2025-05-27 13:47:23 +0700

    1h-27/5

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { UsersModule } from '../users/users.module';
+import { JwtModule } from '@nestjs/jwt';
+import { JWT_SECRET } from '../configs/jwt-secret'; // Adjust the import path as necessary
+
+@Module({
+  providers: [AuthService],
+  controllers: [AuthController],
+  imports: [
+    UsersModule,
+    JwtModule.register({
+      secret: JWT_SECRET,
+      global: true,
+      signOptions: { expiresIn: '1h' }, // Token expiration time],
+    }),
+  ],
+})
+export class AuthModule {}
